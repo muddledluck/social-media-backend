c33f3cde69dd3150e2a28d621ea3afc7ca132e24
Oscar <oscar@superiorcsgroup.com>
2022-07-23 19:22:13 +0530

    fix typo

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -11,7 +11,7 @@ const userCore = {
   name: z
     .string({
       required_error: "Name is required",
-      invalid_type_error: "Password must be a string",
+      invalid_type_error: "Name must be a string",
     })
     .min(1),
 };
